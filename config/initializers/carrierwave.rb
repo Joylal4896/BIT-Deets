fc051aebc72439a56fec41d79b747e34362cb197
Joy Lal Chattaraj <Joylal4896@gmail.com>
2016-07-24 22:31:34 +0530

    Delete carrierwave.rb

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAJDEGFGK34NS3S5GQ',
-      :aws_secret_access_key  => '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',
-      :region                 => 'ap-southeast-1'
-  }
-  config.fog_directory  = 'bitdeets'
-end
